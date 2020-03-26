0178798b02eeac22d548b3cccb579e6f795de1e1
Wesley Marques <wes.academico@gmail.com>
2020-03-26 18:26:23 -0300

    add interface de cadastro de casos

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Logon from './pages/Logon'
 import Register from './pages/Register'
+import Profile from './pages/Profile'
+import NewIncident from './pages/NewIncident'
 
 // Switch garante que uma rota por vez seja chamada
 // exact diz que o caminhho deve ser exatamente a "/" para poder acessala
@@ -14,6 +16,8 @@ export default function Routes() {
             <Switch> 
                 <Route path="/" exact component={Logon} /> 
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     )
